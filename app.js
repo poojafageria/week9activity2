d04bea6ecc5ceaf46b98525530d8edb967198cca
Pooja Rani <pooja.rani@metropolia.fii>
2024-04-08 21:08:08 +0530

    first commit

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const methodOverride = require('method-override')
 const connectDB = require("./config/db");
 const logger = require("./middlewares/logger")
 
-const blogAPI = require("./controllers/locationAPIController");
-const blogSSR = require("./controllers/locationSSRController");
+const blogAPI = require("./controllers/contactAPIController");
+const blogSSR = require("./controllers/contactSSRController");
  
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -26,35 +26,35 @@ connectDB();
  
 // SSR
 
-// End1: Route to render index.html with locations using EJS
-app.get("/", blogSSR.renderLocations);
-// End2: Define a route to render the addlocation.ejs view
-app.get("/addlocation", blogSSR.renderForm);
-// End3:Route to add  location using EJ
-app.post("/addlocation", blogSSR.addLocation);
-// Define a route to render the singlelocation.ejs view
-app.get("/single-location/:id", blogSSR.renderLocation);
-// Define a route to delete singlelocation
-app.delete("/single-location/:id", blogSSR.deleteLocation);
-// Define a route to update single location.ejs
-app.put("/single-location/:id", blogSSR.updateLocation);
-// Define location to update
-app.get("/single-location/update/:id", blogSSR.renderUpdateLocation);
+// End1: Route to render index.html with contacts using EJS
+app.get("/", blogSSR.renderContacts);
+// End2: Define a route to render the addcontact.ejs view
+app.get("/addcontact", blogSSR.renderForm);
+// End3:Route to add  contact using EJ
+app.post("/addcontact", blogSSR.addContact);
+// Define a route to render the singlecontact.ejs view
+app.get("/single-contact/:id", blogSSR.renderContact);
+// Define a route to delete singlecontact
+app.delete("/single-contact/:id", blogSSR.deleteContact);
+// Define a route to update single contact.ejs
+app.put("/single-contact/:id", blogSSR.updateContact);
+// Define contact to update
+app.get("/single-contact/update/:id", blogSSR.renderUpdateContact);
 
 // API
-// GET all Locations
-app.get("/api/locations", blogAPI.getLocations);
-// POST a new Location
-app.post("/api/locations", blogAPI.addLocation);
-// GET a single Location
-app.get("/api/locations/:id", blogAPI.getLocation);
+// GET all Contacts
+app.get("/api/contacts", blogAPI.getContacts);
+// POST a new Contact
+app.post("/api/contacts", blogAPI.addContact);
+// GET a single Contact
+app.get("/api/contacts/:id", blogAPI.getContact);
 
-// Update Location using PUT
-app.put("/api/locations/:id", blogAPI.updateLocation);
-// DELETE a Location
-app.delete("/api/locations/:id", blogAPI.deleteLocation);
-// DELETE all Location
-app.delete("/api/locations", blogAPI.deleteAllLocations);
+// Update Contact using PUT
+app.put("/api/contacts/:id", blogAPI.updateContact);
+// DELETE a Contact
+app.delete("/api/contacts/:id", blogAPI.deleteContact);
+// DELETE all Contact
+app.delete("/api/contacts", blogAPI.deleteAllContacts);
 
 const PORT = 4000;
 
